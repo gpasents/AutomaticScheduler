7ec8cced7d9d0592dc9748713d19af49fd7067db
Giorgos Pasentsis <gpasents@gmail.com>
2021-06-23 18:12:32 +0300

    changed secret key and way of aquiring it

--- project/settings.py
+++ project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tsm$6)maal(_)=kwdl&6fb5_nefe6ky8nt*1ksz_wr2$+m!hc*'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','automaticscheduler.herokuapp.com']
 

d1b4d97ac981558099638fedd7fdfce3c3e204c8
Giorgos Pasentsis <gpasents@gmail.com>
2021-06-24 15:11:22 +0300

    created the first page for the scheduler

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name = "home-page")
+    path('',views.home, name = "scheduler-home-page"),
+    path('first/', views.first, name = "scheduler-first-page")
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Testing stuff</h1>')
+
+def first(request):
+    return HttpResponse('<h1>Testing stuff 2</h1>')

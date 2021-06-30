53e08f98d862a2420b506c1120a5b3b9420204a6
Giorgos Pasentsis <gpasents@gmail.com>
2021-06-30 18:10:19 +0300

    add templates folder and empty home page

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('scheduler/', include('scheduler.urls')),
+    path('', include('scheduler.urls')),
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Testing stuff</h1>')
+    return render(request,'scheduler/home.html')
 
 def first(request):
     return HttpResponse('<h1>Testing stuff 2</h1>')
